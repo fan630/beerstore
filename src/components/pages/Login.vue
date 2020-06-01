241f14a9daa62eb6a75c77c040c62d5eba8f18d8
fan630 <norriswu266@gmail.com>
2020-06-01 23:38:26 +0800

    first release

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <form class="form-signin" @submit.prevent="signin">
+      <h1 class="h3 mb-3 font-weight-normal text-center">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" v-model.trim="user.username" placeholder="Email address" required autofocus >
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" v-model.trim="user.password" placeholder="Password" required >
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return{
+       user:{
+         username: '', 
+         password: ''
+       }, 
+    }
+  }, 
+  methods:{
+    signin(){
+        const api = 'https://vue-course-api.hexschool.io/admin/signin'
+        // const api = `${process.env.VUE_APP_API}/api/fan630/products`
+        // console.log(process.e nv.VUE_APP_API)
+        const vm = this
+        this.$http.post(api, vm.user).then((response) => {
+            console.log(response.data)
+        })
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+html,
+    body {
+    height: 100%;
+    }
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
