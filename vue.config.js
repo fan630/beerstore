0463144d4d4a9e6b817bcdbfd4245e241b0a153f
fan630 <norriswu266@gmail.com>
2020-06-17 18:59:31 +0800

    revise config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/beerstore/'
-        : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/beerstore/' : ''
 }
