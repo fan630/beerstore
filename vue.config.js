47a66d5bd7aa9f33ee3f25bca437b78110bed9de
fan630 <norriswu266@gmail.com>
2020-06-21 19:16:31 +0800

    config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,33 @@
+const express = require('express')
+const app = express()
+var data = require('./public/data.json')  //本地json文件数据
+
+var apiRoutes = express.Router();
+app.use('/api', apiRoutes)
+
+
 module.exports = {
     lintOnSave: false,
     publicPath: process.env.NODE_ENV === 'production'
         ? '/beerstore/'
         : '/'
     ,
+    devServer: {
+        before(app) {
+            app.get('/coupon', (req, res) => {
+                res.json({
+                    errno: 0,   // 这里是你的json内容
+                    data: data
+                })
+            })
+        },
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: null  
+    },
     // pwa: {
     //     iconPaths: {
     //         favicon32: './favicon.ico',
