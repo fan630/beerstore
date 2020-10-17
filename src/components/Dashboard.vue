c70d8521c89e5886b0e01faeb64c0c20b5bf40c1
fan630 <norriswu266@gmail.com>
2020-10-18 01:03:16 +0800

    cookie setting

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-      <Navbar />
-      <AlertMessage></AlertMessage>
-      <div class="container-fluid">
-          <Sidebar/>
-          <div class="row">
+<div>
+    <Navbar />
+    <AlertMessage></AlertMessage>
+    <div class="container-fluid">
+        <Sidebar />
+        <div class="row">
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
                 <router-view></router-view>
             </main>
-          </div>
-      </div>
-  </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -19,11 +19,16 @@ import Navbar from './Navbar.vue';
 import AlertMessage from './AlertMessage.vue';
 
 export default {
-  name: 'Dashboard',
-  components: {
-    Sidebar,
-    Navbar,
-    AlertMessage,
-  },
+    name: 'Dashboard',
+    components: {
+        Sidebar,
+        Navbar,
+        AlertMessage,
+    },
+    created() {
+        const cookieValue = document.cookie.split('; ').find(row => row.startsWith('token')).split('=')[1];
+        // cookie往後端送
+        this.$http.defaults.headers.common.Authorization = cookieValue
+    }
 };
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,93 +1,96 @@
 <template>
-  <div>
+<div>
     <form class="form-signin" @submit.prevent="signin">
-      <img src="../assets/images/logo.png" alt="" srcset="">
-      <h1 class="h3 mb-3 font-weight-normal text-center pt-3">後台管理</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control"
-            v-model.trim="user.username" placeholder="Email address" required autofocus
-      />
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control"
-            v-model.trim="user.password" placeholder="Password" required
-      />
-      <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+        <img src="../assets/images/logo.png" alt="" srcset="">
+        <h1 class="h3 mb-3 font-weight-normal text-center pt-3">後台管理</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" v-model.trim="user.username" placeholder="Email address" required autofocus />
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" v-model.trim="user.password" placeholder="Password" required />
+        <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
     </form>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
-  data() {
-    return {
-      user: {
-        username: '',
-        password: '',
-      },
-    };
-  },
-  methods: {
-    signin() {
-      const api = 'https://vue-course-api.hexschool.io/admin/signin';
-      // const api = `${process.env.VUE_APP_API}/api/fan630/products`
-      // console.log(process.e nv.VUE_APP_API)
-      // const vm = this
-      this.$http.post(api, this.user).then((response) => {
-        console.log(response.data);
-        if (response.data.success) {
-          this.$router.push('/admin/products');
-        }
-      });
+    name: 'Login',
+    data() {
+        return {
+            user: {
+                username: '',
+                password: '',
+            },
+        };
     },
-  },
+    methods: {
+        signin() {
+            const api = 'https://vue-course-api.hexschool.io/admin/signin';
+            this.$http.post(api, this.user).then((response) => {
+                console.log(response.data);
+                if (response.data.success) {
+                    const token = response.data.token;
+                    const expired = response.data.expired;
+                    console.log(token, expired);
+                    document.cookie = `token=${token}; expires=${new Date(expired)}`;
+                    this.$router.push('/admin/products');
+                }
+            });
+        },
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+
+<style lang="scss" scoped>
 html,
-    body {
+body {
     height: 100%;
-    }
+}
 
 body {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
 }
 
 .form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
 }
+
 .form-signin .checkbox {
-  font-weight: 400;
+    font-weight: 400;
 }
+
 .form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
 }
+
 .form-signin .form-control:focus {
-  z-index: 2;
+    z-index: 2;
 }
+
 .form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
 }
+
 .form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
 }
 </style>
